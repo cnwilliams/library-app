3bba3866526d7dec7f7247ec1f19756264ce1062
cnwilliams <hello.christenwilliams@gmail.com>
2017-03-12 13:22:45 -0700

    fixes confirm method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  def confirm
-    self.confirm(params)
+  def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user ? @user.authenticate(params[:password]) : false
   end
